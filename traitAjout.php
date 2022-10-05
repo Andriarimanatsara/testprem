5047fadc5e230a05c7270978541fdd7ca5e50b4d
Andriarimanatsara <97059035+Andriarimanatsara@users.noreply.github.com>
2022-10-05 16:51:24 +0300

    Add files via upload

---
+++ traitAjout.php
@@ -0,0 +1,13 @@
+<?php
+	include('Connexion.php');
+
+    $mysqlClient=connex();
+    $menu=$_POST["nom"];
+    $lien=$_POST["lien"];
+    var_dump($_POST['user'])
+    
+    //var_dump($user);
+    //echo "admin==".$admin;
+    ///echo "user==".$user;
+    
+?>
